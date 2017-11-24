1b511e7a9360479f58c6b20e7b30d8f776505905
print(hello) <almofadex@gamil.com>
2017-11-24 23:03:40 +0000

    rjth

--- src/oopprojectnet/PeopleMenu.java
+++ src/oopprojectnet/PeopleMenu.java
@@ -9,12 +9,12 @@ package oopprojectnet;
  *
  * @author almof
  */
-public class MainMenu extends javax.swing.JFrame {
+public class PeopleMenu extends javax.swing.JFrame {
 
     /**
-     * Creates new form MainMenu
+     * Creates new form PeopleMenu
      */
-    public MainMenu() {
+    public PeopleMenu() {
         initComponents();
     }
 
@@ -34,7 +34,7 @@ public class MainMenu extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new java.awt.GridLayout());
 
-        jButton1.setText("People");
+        jButton1.setText("Create Person");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -42,17 +42,18 @@ public class MainMenu extends javax.swing.JFrame {
         });
         getContentPane().add(jButton1);
 
-        jButton2.setText("Places");
+        jButton2.setText("Edit Person");
         getContentPane().add(jButton2);
 
-        jButton3.setText("Events");
+        jButton3.setText("Delete Person");
         getContentPane().add(jButton3);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        new PeopleMenu().setVisible(true);
+        setContentPane(new CreatePersonPanel());
+        revalidate();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -72,20 +73,20 @@ public class MainMenu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PeopleMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PeopleMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PeopleMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(PeopleMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainMenu().setVisible(true);
+                new PeopleMenu().setVisible(true);
             }
         });
     }
